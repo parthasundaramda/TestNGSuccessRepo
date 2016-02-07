ca60d9c525ba58d5a8412d68d581511664ad82ba
partha sundaram <parthasundarams@gmail.com>
2016-02-08 04:50:42 +0530

    Initial Commit

---
+++ src/test/java/com/testng/maven/testNGMaven/TestNgMavenExampleTest.java
@@ -0,0 +1,12 @@
+package com.testng.maven.testNGMaven;
+
+import org.testng.annotations.Test;
+
+public class TestNgMavenExampleTest {
+    @Test
+    public void exampleOfTestNgMaven() {
+        System.out.println("This is TestNG-Maven Example");
+    }
+}
+
+
